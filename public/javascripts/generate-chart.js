8879b51f1e92c8a49a64963c4a9490d1b211810e
Andrea Lorenzani <work@andrea.lorenzani.name>
2017-03-15 23:37:26 +0000

    Added multiple type of graphs with CanvasJS

--- public/javascripts/generate-chart.js
+++ public/javascripts/generate-chart.js
@@ -1,4 +1,18 @@
 function generateChart(container, type, event, votes) {
+    if(event!==undefined) {
+        window.storedevent=event;
+    }
+    else {
+        event = window.storedevent;
+    }
+
+    if(votes!==undefined) {
+        window.storedvotes=votes;
+    }
+    else {
+        votes = window.storedvotes;
+    }
+
     var chart = new CanvasJS.Chart(container, {
         title:{
             text: event.name
